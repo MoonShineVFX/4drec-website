b2cf90ecaead086ce66074f6ee8eda8d36cc226b
wood <kilokingw@gmail.com>
2021-07-21 12:29:00 +0800

    更換vimeo url

--- src/App/vimeoplayer.js
+++ src/App/vimeoplayer.js
@@ -8,7 +8,7 @@ function vimeoplayer() {
         <div className='player-wrapper'>
           <ReactPlayer
             className='react-player'
-            url='https://vimeo.com/435638545'
+            url='https://vimeo.com/577427935'
             width='100%'
             height='100%'
             controls
